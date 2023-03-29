13463d46bb6ea63f2824627a591044465421968e
Mitchel Ogunniyi <isokandone@gmail.com>
2023-03-29 10:06:20 +0100

    working on the navbar now

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import styles from '../page.module.css'
+
+
+const Navbar = () => {
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.navbar__title}>Site Title</div>
+      <ul className={styles.navbar__navigation}>
+        <li><Link href="/">Home</Link></li>
+        <li><Link href="/about">About</Link></li>
+        <li><Link href="/services">Services</Link></li>
+        <li><Link href="/blog">Blog</Link></li>
+        <li><Link href="/contact">Contact</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
