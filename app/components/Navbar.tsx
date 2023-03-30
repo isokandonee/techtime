ee45c5be88a38250dc782a249357af5d84e11dff
Mitchel Isokan Ogunniyi <isokandone@gmail.com>
2023-03-30 05:25:43 +0000

    update from codesandbox

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,17 +1,32 @@
-import Link from 'next/link';
-import styles from '../page.module.css'
-
+import Link from "next/link";
+import styles from "../page.module.css";
 
 const Navbar = () => {
   return (
     <nav className={styles.navbar}>
       <div className={styles.navbar__title}>Site Title</div>
       <ul className={styles.navbar__navigation}>
-        <li><Link href="/">Home</Link></li>
-        <li><Link href="/about">About</Link></li>
-        <li><Link href="/services">Services</Link></li>
-        <li><Link href="/blog">Blog</Link></li>
-        <li><Link href="/contact">Contact</Link></li>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/services">Services</Link>
+        </li>
+        <li>
+          <Link href="/blog">Blog</Link>
+        </li>
+        <li>
+          <Link href="/contact">Contact</Link>
+        </li>
+        <li>
+          <button className={styles.button}>
+            <Link href="/contact">Click Me</Link>
+          </button>
+          
+        </li>
       </ul>
     </nav>
   );
